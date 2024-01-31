8a30856889e09d6fdc2a1543fe5b43bef484a3f4
pllinas <pllinas42@gmail.com>
2024-01-31 19:58:02 +0100

    feat: :sparkles: Add lesson 4

---
+++ first_lesson/4_asignacion_comprobacion.py
@@ -0,0 +1,32 @@
+# Que diferencia existe entre un igual y dos iguales?
+
+# = VS ==
+
+# = -> Asignacion
+# == -> Comprobacion
+
+# Ejemplo:
+
+a = 10
+b = 20
+
+# Ves descomentandolo poco a poco para ver el resultado de cada linea y ves ejecutando el script
+
+# $ python3 4_asignacion_comprobacion.py
+
+print(a == b) # False
+# print(a = b) # Error
+# print(a = a) # 10
+# print(a == a) # True
+
+# print(a == 10) # True
+# print(a = 10) # Error
+# print(a = 20) # Error
+# print(a == 20) # False
+
+# print(a == b) # False
+# print(a = b) # Error
+# print(a = a) # 10
+# print(a == a) # True
+
+# Extiende el script y trata de hacer tus propias pruebas
