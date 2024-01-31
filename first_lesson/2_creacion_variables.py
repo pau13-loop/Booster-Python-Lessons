b498641e58bd5bdb30677a90464af473cecb5d9f
pllinas <pllinas42@gmail.com>
2024-01-31 19:31:18 +0100

    feat: :sparkles: Add lesson two

---
+++ first_lesson/2_creacion_variables.py
@@ -0,0 +1,18 @@
+# Primeras instrucciones y pruebas con python
+a = 'Hola' # string
+b = 12 # integer
+c = (2 + 2 + 8) # 12
+
+print('Hello world')
+
+print(a)
+print (b + c)
+# print (a + b + c) Error: no respeta el tipo de dato
+
+
+# Modificamos el tipo de dato
+# int() -> convierte a integer
+# str() -> convierte a string
+
+# Resultado: '24'
+print (a + str(b + c))
