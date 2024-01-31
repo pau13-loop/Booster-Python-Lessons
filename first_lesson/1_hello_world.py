1e001b7b606cf437ecd0e2f416e3eea031f936ab
pllinas <pllinas42@gmail.com>
2024-01-31 19:28:18 +0100

    feat: :sparkles: First lesson hello world

---
+++ first_lesson/1_hello_world.py
@@ -0,0 +1,9 @@
+# Ejecutar en la terminal
+# $ python print("Hello world!")
+
+# Ejecutar desde terminal: $ python hello-world.py
+print("Hello world!")
+
+print(2 ** 100)
+print(2024 - 1998)
+print(8)
