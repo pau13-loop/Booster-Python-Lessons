8864bdb127a4ff5ec49f0efb52f0bee6caea6a56
pllinas <pllinas42@gmail.com>
2024-01-31 19:57:52 +0100

    feat: :sparkles: Add lesson 3

---
+++ first_lesson/3_tipos_de_datos.py
@@ -0,0 +1,13 @@
+# Numbers 1234, 3.1415, 3+4j, 0b111, Decimal(), Fraction()
+# Strings 'spam', "Bob's", b'a\x01c', u'sp\xc4m', '123515'
+
+# Que tienen en comun todas las strings? 
+# Siempre van rodeadas de comillas, simples o dobles
+
+# Como podemos conocer el tipo de dato de una variable?
+
+dato_x = 20
+dato_y = '20'
+
+print('esto es: ', type(dato_x)) # Tipo de dato integer
+print('esto es: ', type(dato_y)) # Tipo de dato string
